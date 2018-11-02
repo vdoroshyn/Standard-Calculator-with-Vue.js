04932b9d373844138eaabc411fe7c062d2501197
vdoroshyn <kjioyh9@gmail.com>
2018-11-03 00:10:36 +0200

    the main logic is ready, however, everything is in the view layer and there are bugs, a lot of them

---
+++ src/components/Calculator.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class=calculator>
+    <div class="display">{{current || 0}}</div>
+    <div @click="clear"          class="btn">AC</div>
+    <div @click="changeSign"     class="btn">+/-</div>
+    <div @click="percent"        class="btn">%</div>
+    <div @click="division"       class="btn operator">/</div>
+    <div @click="append('7')"    class="btn">7</div>
+    <div @click="append('8')"    class="btn">8</div>
+    <div @click="append('9')"    class="btn">9</div>
+    <div @click="multiplication" class="btn operator">x</div>
+    <div @click="append('4')"    class="btn">4</div>
+    <div @click="append('5')"    class="btn">5</div>
+    <div @click="append('6')"    class="btn">6</div>
+    <div @click="substraction"   class="btn operator">-</div>
+    <div @click="append('1')"    class="btn">1</div>
+    <div @click="append('2')"    class="btn">2</div>
+    <div @click="append('3')"    class="btn">3</div>
+    <div @click="addition"       class="btn operator">+</div>
+    <div @click="append('0')"    class="btn zero">0</div>
+    <div @click="dot"            class="btn">.</div>
+    <div @click="equals"         class="btn operator">=</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      previous: null,
+      current: "",
+      operator: null,
+      operatorClicked: false
+    };
+  },
+  methods: {
+    clear() {
+      this.current = "";
+      this.operator = null;
+      this.previous = null;
+      this.operatorClicked = false;
+    },
+    changeSign() {
+      /*
+      **fixed the bug with empty string's sign
+      */
+      if (this.current === "") {
+        this.current = "0";
+      }
+      this.current =
+        this.current.charAt(0) === "-"
+          ? this.current.slice(1)
+          : `-${this.current}`;
+    },
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`;
+    },
+    append(number) {
+      if (this.operatorClicked === true) {
+        this.current = "";
+        this.operatorClicked = false;
+      }
+      this.current = `${this.current}${number}`;
+    },
+    dot() {
+      if (this.current.indexOf(".") === -1) {
+        this.append(".");
+      }
+    },
+    setPrevious() {
+      this.operatorClicked = true;
+      this.previous = this.current;
+    },
+    division() {
+      this.operator = (a, b) => a / b;
+      this.setPrevious();
+    },
+    multiplication() {
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
+    },
+    substraction() {
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
+    },
+    addition() {
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
+    },
+    equals() {
+      this.current = `${this.operator(
+        parseFloat(this.previous),
+        parseFloat(this.current)
+      )}`;
+      this.previous = null;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.calculator {
+  margin: 0 auto;
+  width: 300px;
+  /* height: 600px; */
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
+
+.display {
+  grid-column: 1 / 5;
+  background-color: #333;
+  color: white;
+}
+
+.btn {
+  background-color: #f2f2f2;
+  border: 1px solid #999;
+  cursor: pointer;
+}
+
+.operator {
+  background-color: orange;
+  color: white;
+}
+
+.zero {
+  grid-column: 1 / 3;
+}
+</style>
